be4474f0d68d3595031d5ebacc804214178e53a6
ArsenFidan <arsenfidan@outlook.com>
2025-08-01 21:17:06 +0200

    Add files via upload

--- assets/js/leesverder.js
+++ assets/js/leesverder.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
-  $('#leesverderknop').on('click', function(e) {
+  $('.leesverderknop').on('click', function(e) {
     e.preventDefault();
-    $('#extra-text').slideToggle();
+
+    var target = $(this).data('target'); 
+    $(target).slideToggle();
+
     $(this).text(function(i, text){
       return text === "Lees verder" ? "Verberg tekst" : "Lees verder";
+    });
+  });
+});
+
+$(document).ready(function() {
+  $('.leesverderknop2').on('click', function(e) {
+    e.preventDefault();
+
+    var $knop = $(this);
+    var target = $knop.data('target');
+    var origineleTekst = $knop.data('text'); 
+
+    $(target).slideToggle(function() {
+      var isZichtbaar = $(this).is(':visible');
+      $knop.text(isZichtbaar ? 'Verberg tekst' : origineleTekst);
     });
   });
 });
